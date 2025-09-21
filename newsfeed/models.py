b3bd4a94452e75259f22b7af3a727a530c75117f
Paolo <pauloportes.guimalan187@gmail.com>
2025-09-22 01:44:36 +0800

    feat: Newsfeed Emoji, Reaction, Comment Models has been added and initialized

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -14,6 +14,12 @@ def generate_random_digit(digit):
 
 
 class Post(models.Model):
+    PRIVACY_STATUS_CHOICES = [
+        ("public", "Public"),
+        ("private", "Private"),
+        ("custom", "Custom"),
+    ]
+
     post_id = models.CharField(
         max_length=150,
         default=generate_random_digit(25),
@@ -31,7 +37,9 @@ class Post(models.Model):
     caption = models.TextField(blank=True, null=True)
     content_type = models.CharField(max_length=50)
     is_tagged = models.BooleanField(default=False)
-    privacy_status = models.CharField(max_length=50)
+    privacy_status = models.CharField(
+        max_length=50, choices=PRIVACY_STATUS_CHOICES, default="public"
+    )
     is_sponsored = models.BooleanField(default=False)
     is_live = models.BooleanField(default=False)
     on_feed = models.CharField(max_length=50)
@@ -87,3 +95,43 @@ class MapView(models.Model):
     )
     status = models.BooleanField(default=False)
     is_stationary = models.BooleanField(default=True)
+
+
+class Emoji(models.Model):
+    emoji_id = models.CharField(max_length=40, default=uuid.uuid4, primary_key=True)
+    emoji_title = models.CharField(max_length=20, null=False, default="none")
+    emoji_content = models.CharField(max_length=20, null=False)
+    emoji_tags = models.CharField(max_length=1000, null=False)
+    emoji_theme = models.CharField(null=False, default="#7d7d7d")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+
+class Reaction(models.Model):
+    reaction_id = models.CharField(max_length=40, default=uuid.uuid4, primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="reactions")
+    user = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    emoji = models.ForeignKey(Emoji, on_delete=models.DO_NOTHING, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("post", "user")
+
+
+class Comment(models.Model):
+    comment_id = models.CharField(max_length=40, default=uuid.uuid4, primary_key=True)
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.DO_NOTHING, null=True, blank=True
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField(blank=True, null=True)
+    attachment = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_by = models.ForeignKey(
+        Account, on_delete=models.DO_NOTHING, related_name="deleted_by_account"
+    )
+    deleted_at = models.DateTimeField(blank=True, null=True)
