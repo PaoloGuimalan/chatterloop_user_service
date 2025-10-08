f209ff7120d736ddaf6e188b3ef94808905ac7fa
Paolo <pauloportes.guimalan187@gmail.com>
2025-10-08 10:34:00 +0800

    fix: cors error fix has been implemented

--- user_service/settings.py
+++ user_service/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     # Added apps
     "user",
     "community",
@@ -60,7 +61,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = []
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "user_service.urls"
 
