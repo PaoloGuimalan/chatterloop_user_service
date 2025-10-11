36e978a54dff60b07e24a7558ec6ab181aa55888
Paolo <pauloportes.guimalan187@gmail.com>
2025-10-12 00:26:59 +0800

    fix: specified x-access-token allowed header

--- user_service/settings.py
+++ user_service/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,6 +69,10 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = []
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "x-access-token",
+]
 
 ROOT_URLCONF = "user_service.urls"
 
