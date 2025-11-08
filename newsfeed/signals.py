6daf7c369b31538aec0ccd3c2db4da7f690b8c27
Paolo <pauloportes.guimalan187@gmail.com>
2025-11-09 06:51:26 +0800

    fix: signal for creating post preview count on new post creation

--- newsfeed/signals.py
+++ newsfeed/signals.py
@@ -22,8 +22,10 @@ def create_post_preview_counts(sender, instance, created, **kwargs):
 def create_preview_counts_for_new_post(sender, instance, created, **kwargs):
     if created:
         emojis = Emoji.objects.all()
-    preview_counts = [
-        PreviewCount(preview_id=str(uuid.uuid4()), post=instance, emoji=emoji, count=0)
-        for emoji in emojis
-    ]
-    PreviewCount.objects.bulk_create(preview_counts)
+        preview_counts = [
+            PreviewCount(
+                preview_id=str(uuid.uuid4()), post=instance, emoji=emoji, count=0
+            )
+            for emoji in emojis
+        ]
+        PreviewCount.objects.bulk_create(preview_counts)
