d8384f3f811bb3571251faa3d57932880f0cf8a2
Paolo <pauloportes.guimalan187@gmail.com>
2026-04-18 01:19:20 +0800

    feat: added parent object for realm profile info endpoint

--- community/serializers.py
+++ community/serializers.py
@@ -16,12 +16,20 @@ class RealmFollowSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class BasicRealmSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Realm
+        fields = "__all__"
+
+
 class RealmSerializer(serializers.ModelSerializer):
     followers_count = serializers.IntegerField(read_only=True)
     members = serializers.IntegerField(read_only=True)
     is_member = serializers.BooleanField(read_only=True)
     is_admin = serializers.BooleanField(read_only=True)
     is_follower = serializers.BooleanField(read_only=True)
+    parent = BasicRealmSerializer(allow_null=True)
 
     class Meta:
         model = Realm
